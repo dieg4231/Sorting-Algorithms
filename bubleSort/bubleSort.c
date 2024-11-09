88efc5b56946d0efa29d69c1b2f9542425675ed1
Diego Cordero <dieg4231@gmail.com>
2024-11-08 23:48:01 -0600

    util headers added, function to read and print numbers now are in util to reuse in other programs

--- bubleSort/bubleSort.c
+++ bubleSort/bubleSort.c
@@ -1,57 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../util/nums2sort.h"
 
+/*
 void printList(int *numberList, int size){
-	
-	for(int i = 0 ; i < size; i++){
-		printf("%d ",numberList[i]);
-	}	
+	for(int i = 0 ; i < size; i++)
+		printf("%d ",numberList[i]);	
 	printf("\n");
 }
 
 int  readNumbersFromFile( char *fileName, int  maxSize, int **numberList ){
-
 	FILE *file;
 	int count = 0;
 	file = fopen(fileName,"r");
-	
 	printf("Reading file ... \n");
 	if(file == NULL){
 		printf("FILE ERROR\n");
 		return 1;
 	}
-		
+
 	*numberList = (int *)malloc(maxSize * sizeof(int) );
 
 	while(fscanf(file,"%d", (*numberList)+count   ) != EOF  && count < maxSize  )
 		count++;
 	
 	fclose(file);
-
 	printf("Reading file DONE\n");
 	printf("All numbers to sort: %d \n",count );
 	
-
 	return count;
 }
+*/
 
 
 int main(int argc, char **argv){
 
-	
 	int *numberList = NULL;
-        int maxSize = atoi(argv[2]);
+    int maxSize = atoi(argv[2]);
 	int numbersSize;
 	int changes,aux;
 
 	printf("MAX SIZE  %d \n",maxSize);
-
-	numbersSize = readNumbersFromFile( argv[1], maxSize, &numberList );
-	
+	numbersSize = readNumbersFromFile( argv[1], maxSize, &numberList );	
 	printList(numberList, numbersSize);
 	
-	changes = 1;
-	
+	changes = 1;	
 	while(changes != 0){
 		changes = 0;
 		for(int i = 1; i < numbersSize; i++){
@@ -63,10 +56,7 @@ int main(int argc, char **argv){
 				changes++;
 			}  
 		}
-
-
 	}
-
 	printList(numberList, numbersSize);
 	free(numberList);
 	return 0;
