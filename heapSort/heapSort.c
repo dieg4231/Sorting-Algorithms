7fef61c39b93e2df5f19ed391621f2c9b725faf8
dieg4231 <dieg4231@gmail.com>
2024-11-12 18:47:04 -0600

    improving code

--- heapSort/heapSort.c
+++ heapSort/heapSort.c
@@ -3,72 +3,66 @@
 #include <math.h>
 #include "../util/nums2sort.h"
 
-void maxHeapify(int **numberList, int index,int numbersSize){
-	int l,r,largest;
+void maxHeapify(int **numberList, int index, int numbersSize){
 	
-	if (index == 0)
-	{
+	int l,r,largest;	
+	//Left and right index
+	if (index == 0){
 		l = 1;
 		r = 2;
 	}else{
-
 		l = index << 1;
-		r = (index << 1) +1;
+		r = l +1;
 	}	
-	//printf("--------pre----numbersSize = %d indx = %d  l = %d r = %d---\n",numbersSize,index,l,r );
 
-	//printList(*numberList, numbersSize);
-
-	if(l < numbersSize && (*numberList)[l] > (*numberList)[index])
+	//Debug for small list
+	printf("--------pre----numbersSize = %d indx = %d  l = %d r = %d---\n",numbersSize,index,l,r );
+	printList(*numberList, numbersSize);
+	if( l < numbersSize && (*numberList)[l] > (*numberList)[index])
 		largest = l;
 	else
 		largest = index;
-	if(r < numbersSize && (*numberList)[r] > (*numberList)[largest])
+	if( r < numbersSize && (*numberList)[r] > (*numberList)[largest])
 		largest = r;
 	if(largest != index){
 		int aux = (*numberList)[index];
 		(*numberList)[index] = (*numberList)[largest];
 		(*numberList)[largest] = aux;
-		maxHeapify(numberList,largest ,numbersSize);
+		maxHeapify(numberList, largest, numbersSize); // recursive  : D
 	}
 	
-
-	//printf("--------post-------\n");
-	
-	///printList(*numberList, numbersSize);
-
 	return;
 }
 
-void maxHeap(int **numberList,int numbersSize){
-
-	for(int i = floor(numbersSize/2); i >=0 ;i--)
+//Create a max heap
+void maxHeap(int **numberList, int numbersSize){
+	for(int i = floor( numbersSize / 2 ); i >= 0; i--)
 		maxHeapify(numberList,i,numbersSize);
-
 	return;
 }
 
 int main(int argc, char **argv){
 	
 	int *numberList = NULL;
-        int maxSize = atoi(argv[2]);
+    int maxSize = atoi(argv[2]);
 	int numbersSize;
 	int changes,aux;
 
 	printf("MAX SIZE  %d \n",maxSize);
-
-	numbersSize = readNumbersFromFile( argv[1], maxSize, &numberList );	
+	numbersSize = readNumbersFromFile(argv[1], maxSize, &numberList);	
 	printList(numberList, numbersSize);
-
-	maxHeap(&numberList,numbersSize);
+	
+	//Init sorting
+	maxHeap(&numberList, numbersSize); // Get a max heap
 	int auxNumbersSize = numbersSize;
-	for(int i = numbersSize; i > 0; i--){
-		int aux = numberList[0];
-		numberList[0] = numberList[i];
-		numberList[i] = aux;
-		maxHeapify(&numberList,0, --auxNumbersSize);
 
+	for(int i = numbersSize-1; i > 0; i--){
+		int aux = numberList[0];
+		numberList[0] = numberList[i]; // Swap root vs i node
+		numberList[i] = aux;
+		maxHeapify(&numberList,0, --auxNumbersSize); // call to heapify to ensure  the heap is a max heap 
 	}
+	//End sorting
 
 	printList(numberList, numbersSize);
 	free(numberList);
